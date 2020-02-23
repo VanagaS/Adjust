622d119535572fab11553910edf5920a1fc3c325
Aditya Naga Sanjeevi, Yellapu <sanjeev.yellapu@cultuzz.com>
2020-02-23 17:57:55 +0000

    Added logic to not to print consecutive numbers

--- go-cli/go-cli.go
+++ go-cli/go-cli.go
@@ -12,12 +12,18 @@ func o_of_n_time_o_of_n_space(size int) {
 	// create a map to contain the random numbers
 	mapp := make(map[int]bool)
 
+	// consider a case, where consecutive numbers are not printed
+	previous := 0
+
+	// Print the numbers from 1 to 10 in random order to the terminal.
 	for len(mapp) != size {
 		// get a random integer within given size (10, here)
+		// note: default rand is deterministic
 		k := rand.Intn(size + 1)
 
-		// we do not want 0 printed
-		if k == 0 {
+		// we do not want 0 printed nor the next and previous values of previous number
+		// this would normally add an overhead.
+		if k == 0 || previous == k || previous == k+1 || previous == k-1 {
 			continue
 		}
 
@@ -25,6 +31,9 @@ func o_of_n_time_o_of_n_space(size int) {
 		if !mapp[k] {
 			// print the number
 			print(k, "\n")
+
+			// save the number as the previous number
+			previous = k
 		}
 
 		// save the number
